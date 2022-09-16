8d196796b37d70ccc3dafe4b8462c7faa0dbece0
mdimran1409036 <mdimran1409036@gmail.com>
2022-09-17 02:25:09 +0600

    duplicate data upload resolved

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,5 @@
 const db = require("../db/db");
+var mysql = require("mysql");
 const fs = require("fs");
 const { getComments } = require("../lib/getComments");
 const handleFileUpload = (req, res) => {
@@ -6,7 +7,7 @@ const handleFileUpload = (req, res) => {
     const data = require(`../uploads/${fileName}`);
     //creating comments table
     let sql =
-        "CREATE TABLE IF NOT EXISTS comments(id int AUTO_INCREMENT, body VARCHAR(255) NOT NULL, postId int NOT NULL, userId int NOT NULL, username VARCHAR(255) NOT NULL, PRIMARY KEY(id))"; //create table statement
+        "CREATE TABLE IF NOT EXISTS comments(id int, body VARCHAR(255) NOT NULL, postId int NOT NULL, userId int NOT NULL, username VARCHAR(255) NOT NULL, PRIMARY KEY(id))"; //create table statement
     //executing query
     db.query(sql, (err) => {
         if (err) throw err;
@@ -14,26 +15,30 @@ const handleFileUpload = (req, res) => {
         const sqlComments = "SELECT * FROM comments";
         db.query(sqlComments, (err, result) => {
             if (err) throw err;
+            
             //comparing the values between database comments and uploaded comments, returning only differences
-            const queryArr = [
+            const queryArrForInsert = [
                 data.comments
                     .filter(
-                        ({ body: id1 }) =>
-                            !result.some(({ body: id2 }) => id2 === id1)
+                        ({ id: id1 }) =>
+                            !result.some(({ id: id2 }) => id1 === id2)
                     )
                     .map((field) => [
+                        field.id,
                         field.body,
                         field.postId,
                         field.user.id,
                         field.user.username,
                     ]),
             ];
-            //only if we have unique comments we will add them into data base
-            if (queryArr[0].length>0) {
+
+
+            //if new data are added in the response.json
+            if (queryArrForInsert[0].length > 0) {
                 let sqlInsert =
-                    "INSERT IGNORE INTO comments (body, postId, userId, username) VALUES ?";
+                    "INSERT IGNORE INTO comments (id, body, postId, userId, username) VALUES ?";
                 //executing the insert query
-                db.query(sqlInsert, queryArr, function (err, result1) {
+                db.query(sqlInsert, queryArrForInsert, function (err, result1) {
                     if (err) throw err;
                     fs.unlinkSync(`./uploads/${fileName}`); //deleting the file from the server after successfully inserted the files into the database
                     res.status(201).json({
@@ -41,8 +46,9 @@ const handleFileUpload = (req, res) => {
                         result1,
                     });
                 });
-            }else{
-                res.status(200).json({message: "All data are duplicate!"})
+            } else {
+                fs.unlinkSync(`./uploads/${fileName}`); //deleting the file from the serer
+                res.status(200).json({ message: "All data are duplicate!" });
             }
         });
     });
--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DB 
+    database: process.env.DB,
+    multipleStatements: true, 
 });
 module.exports = db
